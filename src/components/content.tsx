d72a5bc3782aa44afb2d2bd1fa94dddd7b8b5c39
ArnoldaZ <c.arnas123@gmail.com>
2022-04-27 22:30:25 +0300

    update button css

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -3,10 +3,8 @@ import NotInterestedIcon from "../images/not-interested.svg";
 
 export default function Content(): JSX.Element {
     return (
-        <div id={"content-test"}>
-            <button className="not-interested-button">
-                <img src={NotInterestedIcon} style={{filter: "invert(48%) sepia(79%) saturate(2476%) hue-rotate(86deg) brightness(118%) contrast(119%)"}} />
-            </button>
-        </div>
+        <button className="not-interested-button">
+            <img src={NotInterestedIcon} />
+        </button>
     );
 }
